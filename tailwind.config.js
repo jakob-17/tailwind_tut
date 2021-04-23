7c00f7a8b52b300e4ca3397e7d29c39ab21966f5
Jakob Horvath <jakob.horvath@energyexemplar.com>
2021-04-23 12:02:53 -0600

    messing around w/ forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-    },
+    // container: {
+    //   center: true,
+    //   padding: '2rem',
+    // },
+    extend: {
+      backgroundImage: theme => ({
+        'sanchez': "url('/dist/img/rick-sanchez-png-8.jpg')",
+        'beach': "url('/dist/img/florida-beach.jpg')"
+      })
+    }
   },
   variants: {
     extend: {},
